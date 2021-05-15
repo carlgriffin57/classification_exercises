25666229393478f881961589cee5d9033111498a
Carl Griffin <carl.griffin@yahoo.com>
2021-05-14 19:29:41 -0500

    prepare functions

---
+++ prepare.py
@@ -0,0 +1,9 @@
+def prep_iris(df):
+    '''
+    This function accepts the untransformed iris data and returns the data with tranformations applied.
+    '''
+    df = df.drop(columns=(['species_id', 'measurement_id']))
+    df = df.rename(columns = {'species_name':'species'})
+    dummy_df = pd.get_dummies(df[['species']], dummy_na = False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
