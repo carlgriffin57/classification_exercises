29f90d24174d93abefef6e625ce3509a9b7f2d95
Carl Griffin <carl.griffin@yahoo.com>
2021-05-17 20:03:53 -0500

    fixed runtime error

--- prepare.py
+++ prepare.py
@@ -1,6 +1,11 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, username, password
+
 def prep_iris(df):
     '''
-    This function accepts the untransformed iris data and returns the data with tranformations applied.
+        This function accepts the untransformed iris data and returns the data with tranformations applied.
     '''
     df = df.drop(columns=(['species_id', 'measurement_id']))
     df = df.rename(columns = {'species_name':'species'})
